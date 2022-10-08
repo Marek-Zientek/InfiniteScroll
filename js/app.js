8b6f9f02ad3eb9fe8c05beff368b41a2b6c94367
Marek Zientek <marek.zientek@drupeo.pl>
2022-10-08 15:29:10 +0200

    js / app.js Added functionality to display posts on the page.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 class InfiniteScroll {
-    constructor() {
+    constructor(container) {
+        this.container = container;
         this.init();
     }
 
@@ -7,19 +8,30 @@ class InfiniteScroll {
         window.onload = this.getData;
     }
 
-    async getData() {
+    getData = async () => {
         // Adres serwera
         const apiUrl = `https://jsonplaceholder.typicode.com/posts?_page=1&_limit=4`;
 
         try {
             const res = await fetch(apiUrl);
             const photosArray = await res.json();
-            console.log(photosArray);
+            this.displayPosts(photosArray);
         } catch(err) {
             console.log(err);
         }
     }
+    // wyświetalnie danych z serwera
+    displayPosts = (posts) => {
+        this.container.innerHTML += posts.map( post => {
+            return `
+                <div>
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                </div>
+            `;
+        } ).join("");
+    }
 }
 
 // instancja klasy InfiniteScroll
-const iScroll = new InfiniteScroll();
+const iScroll = new InfiniteScroll(document.querySelector(".container"));
